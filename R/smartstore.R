ae2822312fddbc70a2714ac4668023459b5de6c8
DRTAG <masan.korea@gmail.com>
2021-04-09 20:02:26 +0900

    Updated

--- R/smartstore.R
+++ R/smartstore.R
@@ -9,15 +9,37 @@ getUrlNs <- function() "https://search.shopping.naver.com/allmall"
 
 # Control -----------------------------------------------------------------
 
+
+
+# Static page experiment --------------------------------------------------
+
+categoryClothing <- function(u) paste(u,'repCatNm=CLOTHING',sep='&')
+categoryShoes <- function(u) paste(u,'repCatNm=SHOES',sep='&')
+categoryCosmetics <- function(u) paste(u,'repCatNm=COSMETICS',sep='&')
+categoryLiving <- function(u) paste(u,'repCatNm=LIVING',sep='&')
+categoryFood <- function(u) paste(u,'repCatNm=FOOD',sep='&')
+categoryParenting <- function(u) paste(u,'repCatNm=PARENTING',sep='&')
+categorySports <- function(u) paste(u,'repCatNm=SPORTS',sep='&')
+categoryDigital <- function(u) paste(u,'repCatNm=DIGITAL',sep='&')
+categoryEtc <- function(u) paste(u,'repCatNm=ETC',sep='&')
+
+#static test
+collectNssItems <- function(p=1,category=NULL) {
+  u=sprintf('https://search.shopping.naver.com/allmall/api/allmall?page=%d&sortingOrder=prodClk&isSmartStore=Y',p)
+  if(!is.null(category)) {
+    u=category(u) 
+  }
+  page=read_html(u)
+  page %>% html_element('p') %>% html_text() %>% fromJSON()
+}
+
+
+
+# Dynamic page experiment -------------------------------------------------
+
 checkSmartStore <- function(b) {
   code='#__next > div > div:nth-child(2) > div.mallList_mall_list__20gDk > div > div.mallFilter_btn_area__1uYIL > a.mallFilter_btn_smart__2AHAj'
   b %>% click(code)
-}
-#static test
-collectNssItems <- function(p=1) {
-  u=sprintf('https://search.shopping.naver.com/allmall/api/allmall?page=%d&sortingOrder=prodClk&isSmartStore=Y',p)
-  page=read_html(u)
-  page %>% html_element('p') %>% html_text() %>% fromJSON()
 }
 #dynamic
 collectNssItems2 <- function(b,p=1) {
@@ -30,13 +52,13 @@ collectNssItems2 <- function(b,p=1) {
   return(x2)
 }
 
-collect_smart_store<-function(stopping=NULL,politely=2) {
+collect_smart_store<-function(stopping=NULL,politely=2,category=NULL) {
   items_collected = list()
   item=list()
   page=1
   while(TRUE) {
     cat("Page..",page)
-    item=collectNssItems(page)
+    item=collectNssItems(page,category)
     Sys.sleep(politely)
     if(length(item)<=0) {
       cat("no more page.\n")
--- tester.R
+++ tester.R
@@ -3,5 +3,20 @@ source("R/browser.R")
 source("R/smartstore.R")
 
 # Tester ------------------------------------------------------------------
-
+## Data collection test - for all category
 smartstores=collect_smart_store()
+saveRDS(smartstores,'smartstores.RDS')
+
+## Data collection test - clothing
+smartstores.cloting <- collect_smart_store(politely = 2,                #3 seconds wait
+                                           category = categoryClothing) #clothing up to 100 pages (Naver provided in limit)
+saveRDS(smartstores.cloting,'smartstores.cloting.RDS')
+
+smartstores.shoes <- collect_smart_store(politely = 2,                #3 seconds wait
+                                           category = categoryShoes) #shoes up to 100 pages (Naver provided in limit)
+saveRDS(smartstores.shoes,'smartstores.shoes.RDS')
+
+smartstores.cosmetics <- collect_smart_store(politely = 2,                #3 seconds wait
+                                         category = categoryCosmetics) #cosmetics up to 100 pages (Naver provided in limit)
+saveRDS(smartstores.cosmetics,'smartstores.cosmetics.RDS')
+
